3f4ebc302bba59e28d8fe0af6a45f3f4ccd7f551
Shashank Rai <shashank.xx92@gmail.com>
2022-10-23 00:27:24 +0530

    big data project

--- src/main/scala/bigdata/TaxiApplication.scala
+++ src/main/scala/bigdata/TaxiApplication.scala
@@ -1,7 +1,7 @@
 package bigdata
 
-import org.apache.spark.sql.functions
-import org.apache.spark.sql.functions.hour
+import org.apache.spark.sql.{Column, functions}
+import org.apache.spark.sql.functions.{hour, mean, not, stddev}
 import utils.PathGenerators.getPathResourcesMainFolderWithFile
 import utils.SparkUtils
 import utils.SparkUtils.LOCAL
@@ -11,7 +11,7 @@ object TaxiApplication extends App {
   val spark = SparkUtils.getSparkSession("Big_Data_Application", LOCAL)
   import spark.implicits._
 
-  val bigTaxiDF  = spark.read.parquet("C:\\Users\\shash\\Downloads\\NYC_taxi_2009-2016.parquet")
+  //val bigTaxiDF  = spark.read.parquet("C:\\Users\\shash\\Downloads\\NYC_taxi_2009-2016.parquet")
   //val taxiDF = bigTaxiDF
   val taxiDF = spark.read.parquet(getPathResourcesMainFolderWithFile("yellow_taxi_jan_25_2018"))
   taxiDF.printSchema()
@@ -111,5 +111,164 @@ object TaxiApplication extends App {
    * +-----------+----------+
    * */
 
+  // 3
+  val tripDistanceDF = taxiDF.select($"trip_distance".as("distance"))
+  val longDistanceThreshold = 30
+  val tipsDistanceStatsDF = tripDistanceDF.select(
+    functions.count("*") as "count",
+    mean("distance") as "mean",
+    stddev("distance") as "stddev",
+    functions.min("distance") as "min",
+    functions.max("distance") as "max"
+  )
 
+  /**
+   * +------+-----------------+-----------------+---+----+
+   * | count|             mean|           stddev|min| max|
+   * +------+-----------------+-----------------+---+----+
+   * |331893|2.717989442380494|3.485152224885052|0.0|66.0|
+   * +------+-----------------+-----------------+---+----+
+   * */
+
+  val tripsWithLengthDF = taxiDF.withColumn("isLong", $"trip_distance" >= longDistanceThreshold)
+  val tripsByLengthDF = tripsWithLengthDF.groupBy("isLong").count()
+
+  /**
+   * +------+------+
+   * |isLong| count|
+   * +------+------+
+   * |  true|    83|
+   * | false|331810|
+   * +------+------+
+   * */
+
+  // 4
+  val pickUpByHourByLengthDF = tripsWithLengthDF.withColumn("hour_of_day", hour($"tpep_pickup_datetime"))
+    .groupBy("hour_of_day", "isLong")
+    .agg(functions.count("*") as "totalTrips")
+    .orderBy($"totalTrips".desc)
+  pickUpByHourByLengthDF.show(48)
+
+  /**
+   * +-----------+------+----------+
+   * |hour_of_day|isLong|totalTrips|
+   * +-----------+------+----------+
+   * |         22| false|     22104|
+   * |         21| false|     20921|
+   * |         23| false|     20897|
+   * |          1| false|     20824|
+   * |          0| false|     20412|
+   * |         18| false|     18315|
+   * |         11| false|     18264|
+   * |         12| false|     17980|
+   * |          2| false|     17094|
+   * |         19| false|     16858|
+   * |         17| false|     16734|
+   * |         20| false|     16635|
+   * |         15| false|     16190|
+   * |         13| false|     15985|
+   * |         16| false|     15610|
+   * |         14| false|     15158|
+   * |         10| false|     11961|
+   * |          3| false|     10853|
+   * |          9| false|      5358|
+   * |          4| false|      5124|
+   * |          5| false|      3194|
+   * |          6| false|      1971|
+   * |          8| false|      1803|
+   * |          7| false|      1565|
+   * |          0|  true|         9|
+   * |          1|  true|         7|
+   * |         17|  true|         7|
+   * |         11|  true|         6|
+   * |         23|  true|         6|
+   * |         19|  true|         4|
+   * |         14|  true|         4|
+   * |         22|  true|         4|
+   * |         15|  true|         4|
+   * |         10|  true|         3|
+   * |          2|  true|         3|
+   * |         20|  true|         3|
+   * |          3|  true|         3|
+   * |         16|  true|         3|
+   * |          4|  true|         3|
+   * |         12|  true|         3|
+   * |         21|  true|         3|
+   * |         13|  true|         3|
+   * |          6|  true|         2|
+   * |          5|  true|         2|
+   * |         18|  true|         1|
+   * +-----------+------+----------+
+   * */
+
+  // 5
+  def pickupDropOffPopularity(predicate: Column) = tripsWithLengthDF
+    .where(predicate)
+    .groupBy("PULocationID", "DOLocationID").agg(functions.count("*") as "totalTrips") // count by tuple pickup - dropOff
+    .join(taxiZoneDF, $"PULocationID" === $"LocationID")   // join based on pickup
+    .withColumnRenamed("Zone", "Pickup_zone")  // get pickup zone details
+    .drop("LocationID", "Borough", "service_zone")   // remove other column
+    .join(taxiZoneDF, $"DOLocationID" === $"LocationID")   // join based on drop off
+    .withColumnRenamed("Zone", "DropOff_zone")  // get pickup zone details
+    .drop("LocationID", "Borough", "service_zone")  // remove other column
+    .drop("PULocationID", "DOLocationID")  // remove IDs
+    .orderBy($"totalTrips".desc)
+
+  pickupDropOffPopularity($"isLong").show()
+
+  /**
+   * +----------+--------------------+--------------------+
+   * |totalTrips|         Pickup_zone|        DropOff_zone|
+   * +----------+--------------------+--------------------+
+   * |        14|         JFK Airport|                  NA|
+   * |         8|   LaGuardia Airport|                  NA|
+   * |         4|         JFK Airport|         JFK Airport|
+   * |         4|         JFK Airport|      Newark Airport|
+   * |         3|                  NV|                  NV|
+   * |         3|       Midtown South|                  NA|
+   * |         2|       Midtown North|      Newark Airport|
+   * |         2|         JFK Airport|Riverdale/North R...|
+   * |         2|Penn Station/Madi...|                  NA|
+   * |         2|        Clinton East|                  NA|
+   * |         2|   LaGuardia Airport|      Newark Airport|
+   * |         1|Financial Distric...|         JFK Airport|
+   * |         1|         JFK Airport|         Fort Greene|
+   * |         1|         JFK Airport|Van Nest/Morris Park|
+   * |         1| Little Italy/NoLiTa|Charleston/Totten...|
+   * |         1|         JFK Airport|Eltingville/Annad...|
+   * |         1|         JFK Airport|       Arden Heights|
+   * |         1|         JFK Airport|Prospect-Lefferts...|
+   * |         1|      Midtown Center|Charleston/Totten...|
+   * |         1|            Flushing|                  NA|
+   * +----------+--------------------+--------------------+
+   *
+   * */
+
+  pickupDropOffPopularity(not($"isLong")).show()
+  /**
+   * +----------+--------------------+--------------------+
+   * |totalTrips|         Pickup_zone|        DropOff_zone|
+   * +----------+--------------------+--------------------+
+   * |      5558|                  NV|                  NV|
+   * |      2425|Upper East Side S...|Upper East Side N...|
+   * |      1962|Upper East Side N...|Upper East Side S...|
+   * |      1944|Upper East Side N...|Upper East Side N...|
+   * |      1928|Upper East Side S...|Upper East Side S...|
+   * |      1052|Upper East Side S...|      Midtown Center|
+   * |      1012|Upper East Side S...|        Midtown East|
+   * |       987|      Midtown Center|Upper East Side S...|
+   * |       965|Upper West Side S...|Upper West Side N...|
+   * |       882|      Midtown Center|      Midtown Center|
+   * |       865|     Lenox Hill West|Upper East Side N...|
+   * |       850|Penn Station/Madi...|      Midtown Center|
+   * |       828|Upper West Side N...|Upper West Side S...|
+   * |       824|Upper West Side S...| Lincoln Square East|
+   * |       809| Lincoln Square East|Upper West Side S...|
+   * |       808|     Lenox Hill West|Upper East Side S...|
+   * |       797|        Midtown East|         Murray Hill|
+   * |       784|Upper East Side S...|     Lenox Hill West|
+   * |       763|      Yorkville West|Upper East Side N...|
+   * |       757|Times Sq/Theatre ...|Penn Station/Madi...|
+   * +----------+--------------------+--------------------+
+   * */
 }
